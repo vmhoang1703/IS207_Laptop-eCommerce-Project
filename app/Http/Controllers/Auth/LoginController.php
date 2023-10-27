ba61b283378af2d13be90496d802a2468f1e3c3e
vmhoang1703 <vmhoang1703@gmail.com>
2023-10-27 21:06:10 +0700

    Completed the basic of Login function

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
@@ -14,5 +16,22 @@ class LoginController extends Controller
     public function showForm(): View
     {
         return view('auth.login');
+    }
+
+    public function sendForm(Request $request)
+    {
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $user = User::where('username', $username)->first();
+
+        if ($user && password_verify($password, $user->password)) {
+            // Đăng nhập thành công
+            Auth::login($user);
+            return redirect('/');
+        } else {
+            // Đăng nhập thất bại
+            return redirect('/login')->with('error', 'Invalid username or password');
+        }
     }
 }
