1cce23f115112b2e12e005b01b6bb98115c4b4d6
MinhAnh.Manh <nguyenanh.3566@gmail.com>
2023-11-16 20:04:11 +0700

    update footer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,6 +59,7 @@
         </div>
     </div>
     @include('components.footer')
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
