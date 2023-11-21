287de8e824704ac0f160a44187fdd5524dca4f46
MinhAnh-Manh <nguyenanh.3566@gmail.com>
2023-11-22 03:20:41 +0700

    responsive order page

--- resources/views/website/oder_process/order_show.blade.php
+++ resources/views/website/oder_process/order_show.blade.php
@@ -12,12 +12,12 @@
 
 <body class="d-flex flex-column">
     <!-- header  -->
-    <!-- <div class="header-fake"></div> -->
+    <div class="header-fake"></div>
      <!-- Header -->
-    @component('components.header')
-    @endcomponent
+    <!-- @component('components.header')
+    @endcomponent -->
     <!-- breadcrumb -->
-    <div class="container-xxl container-sm container-lg container-md ">
+    <div class="container-xxl container-xl container-lg container-md  container-sm   ">
         <ul id="myBreadcrumb" class="breadcrumb">
             <li class="breadcrumb-item"><a href="#">Cửa hàng </a></li>
             <li class="breadcrumb-item"><a href="#">Laptop </a></li>
@@ -27,69 +27,93 @@
     </div>
     <form id="checkoutForm" action="">
         <!-- Nội dung chính -->
-        <div class="container-xxl container-sm container-lg container-md d-flex flex-column ">
+        <div class="container-xxl container-xl container-lg container-md  container-sm d-flex flex-column margin-left-tablet-42   ">
             <!-- Dòng tiêu đề bảng -->
-            <div class="cart-main-section mb-sm-4 mb-lg-4 mb-md-4  mb-xxl-4 row w-100">
-                <div class="text-xl-start col-sm-4 col-lg-4 col-md-4 col-xl-4 ps-5 ">Product</div>
-                <div class="text-center col-sm-3 col-md-3 col-lg-3  col-xl-3 ">Price</div>
-                <div class="text-center col-sm-2 col-md-2 col-lg-2 col-xl-2">Quantity</div>
-                <div class="text-center col-sm-3 col-md-3 col-lg-3 col-xl-3">Subtotal</div>
+            <div class="cart-main-section mb-4 row w-100 mobile-font-size-14">
+                <div class="text-xl-start col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4  ps-5 ">Product</div>
+                <div class="text-center col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3 ">Price</div>
+                <div class="text-center col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-2">Quantity</div>
+                <div class="text-center col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">Subtotal</div>
             </div>
-            <!-- khung chức các sản phẩm đã đặt -->
-            <div class="d-flex flex-column  ">
+            <!-- khung chứa các sản phẩm đã đặt -->
+            <div class="d-flex flex-column margin-top-mobile-16  ">
                 <!-- Dòng thông tin 1 sản phẩm -->
-                <div class="cart-section mb-sm-1 mb-md-2 mb-lg-2 mb-xxl-2 row  w-100">
-                    <a class="card flex-row col-sm-4 col-md-4 col-lg-4 col-xl-4" id="product-img" href="">
+                <div class="cart-section mb-2 row  w-100 mobile-font-size-14">
+                    <a class="card flex-row col-xxl-4 col-xl-4  col-lg-4 col-md-4 col-sm-4  " id="product-img" href="">
                         <img src="https://via.placeholder.com/150" class="card-img-left product-img">
                         <div class="card-body">
-                            <p id="product-name" class="card-title product-title ">Dell XPS 9710 17 inch Core i7</p>
+                            <p id="product-name" class="card-title product-title mobile-font-size-14 ">Dell XPS 9710 17 inch Core i7</p>
                         </div>
                     </a>
-                    <div class="text-center col-sm-3 col-md-3 col-lg-3 col-xl-3 d-flex align-items-center justify-content-center">$<div
+                    <div class="text-center  col-xxl-3 col-xl-3  col-lg-3  col-md-3 col-sm-3  d-flex align-items-center justify-content-center ">$<div
                             id=" product-price">650</div>
                     </div>
-                    <div class="text-center col-sm-2 col-md-2 col-lg-2 col-xl-2">
-                        <input type="number" class="input-quantity " id="quantityInput" value="1" min="1">
+                    <div class="text-center col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-2   ">
+                        <input type="number" class="input-quantity mobile-font-size-14 " id="quantityInput" value="1" min="1">
                     </div>
-                    <div class="text-center col-sm-3 col-md-3 col-lg-3 col-xl-3 d-flex align-items-center justify-content-center">$<div
+                    <div class="text-center col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3  d-flex align-items-center justify-content-center">$<div
                             id=" subtotal-price">2000</div>
                     </div>
                 </div>
 
-                <!--  -->
+                <!-- Mobile  -->
+
+                <div class="cart-section-mobile d-none  w-100 mobile-font-size-14">
+                <a class=" " id="product-img" href="">
+                        <img src="https://via.placeholder.com/150" style="width: 70px " class="card-img-left product-img">
+                </a>
+                <div class=" cart-mobile-infor   ">
+                    <a class="cart-mobile-infor-title d-flex justify-content-between align-items-start" id="product-name-mobile" > Dell XPS 9710 17 inch Core i7 </a>
+                    <div class="  d-flex justify-content-between align-items-end ">
+                        <div class="cart-mobile-infor-price"  id=" product-price-mobile" > 650$ </div>
+                        <div class="input-quantity-mobile d-flex">
+                            <span class="minus d-flex justify-content-center align-items-center" onclick="decreaseQuantity()" >-</span> 
+                            <input type="text" value="1" readonly="readonly" id="quantityInput-mobile"> 
+                            <span class="plus d-flex justify-content-center align-items-center" onclick="increaseQuantity()">+</span>
+                        </div>
+                    </div>
+
+                </div>
+                </div>
+
+
+             
+
+
+               
 
             </div>
-            <div class="   w-100  payment-infor row mt-xl-5 ">
+            <div class="w-100 payment-infor row mt-5 tablet-mt-14 margin-top-mobile-16 margin-left-mobile-none ">
 
-                <div class="coupon-code col-12 col-md-6 px-0 pe-md-5">
+                <div class="coupon-code  col-xxl-5 col-xl-5 col-lg-5 col-md-12 col-sm-12 col-12 px-0 pe-5 padding-right-mobile-none">
                     <div class="input-group">
-                        <input id="couponInput" type="text" class="form-control" placeholder="Coupon Code"
+                        <input id="couponInput mobile-font-size-14 " type="text" class="form-control" placeholder="Coupon Code"
                             aria-label="Coupon Code">
-                        <button class="coupon-btn btn btn-outline-secondary" type="button" id="button-coupon">
+                        <button class="coupon-btn btn btn-outline-secondary mobile-font-size-14 " type="button" id="button-coupon">
                             Apply Coupon
                         </button>
                     </div>
                 </div>
-                <div class=" total-infor pt-3 pb-4 col-xl-6 ms-auto ">
+                <div class=" total-infor pt-3 pb-4 col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 ms-auto margin-top-mobile-16">
                     <div class="container mt-3 ">
                         <div class="cart-total"> Cart Total </div>
                         <div class="total-infor__subtotal row mt-2 ">
-                            <div class=" col-6 text-start"> Subtotal: </div>
-                            <div id="total-infor__subtotal " class=" col-6 text-end"> $2000 </div>
+                            <div class=" col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6  text-start mobile-font-size-14"> Subtotal: </div>
+                            <div id="total-infor__subtotal " class=" col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 text-end mobile-font-size-14"> $2000 </div>
                         </div>
                         <div class="line-grey mt-2 mb-3"></div>
                         <div class="total-infor__shipping row mt-2 ">
-                            <div class=" col-6 text-start"> Shipping: </div>
-                            <div id="total-infor__shipping " class=" col-6 text-end"> Free </div>
+                            <div class=" col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 text-start mobile-font-size-14"> Shipping: </div>
+                            <div id="total-infor__shipping " class=" col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 text-end mobile-font-size-14"> Free </div>
                         </div>
                         <div class="line-grey mt-2 mb-3"></div>
                         <div class="total-infor__total row mt-2 ">
-                            <div class=" col-6 text-start"> Total: </div>
-                            <div id="total-infor__total " class=" col-6 text-end"> $2000 </div>
+                            <div class=" col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 text-start mobile-font-size-14"> Total: </div>
+                            <div id="total-infor__total " class=" col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 text-end mobile-font-size-14"> $2000 </div>
                         </div>
                         <div class="d-flex justify-content-center">
                             <button id="total-infor-btn__checkout"  type="button"
-                                class="total-infor__checkout btn btn-danger  mt-3 ">Checkout</button>
+                                class="total-infor__checkout btn btn-danger  mt-3 mobile-font-size-14 ">Checkout</button>
                         </div>
                     </div>
                 </div>
@@ -100,10 +124,25 @@
 
 
 
-
-
     <div class="footer-fake mt-5"></div>
-    <script src="./index.js"> </script>
+    <script>
+        // Tăng giảm số lượng trong giao diện mobile
+        function decreaseQuantity() {
+            var quantityInput = document.getElementById('quantityInput-mobile');
+            var currentValue = parseInt(quantityInput.value, 10);
+    
+            if (currentValue > 1) {
+                quantityInput.value = currentValue - 1;
+            }
+        }
+    
+        function increaseQuantity() {
+            var quantityInput = document.getElementById('quantityInput-mobile');
+            var currentValue = parseInt(quantityInput.value, 10);
+    
+            quantityInput.value = currentValue + 1;
+        }
+    </script>
 </body>
 
 </html>
