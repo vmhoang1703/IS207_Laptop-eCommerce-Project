0ae945df4f77083d5697ceef20ea05c88a34ef15
vmhoang1703 <vmhoang1703@gmail.com>
2023-12-16 12:07:49 +0700

    fix

--- resources/views/website/cartlist.blade.php
+++ resources/views/website/cartlist.blade.php
@@ -18,9 +18,9 @@
     @endcomponent
 
     <form id="myForm" action=" " class="container-xxl  container-xcontainer-lg  container-md container-sm  list-item d-flex flex-column  gap-2">
-        <div  class=" d-flex row mt-3 mb-3 container-xxl container-xl container-lg  container-md container-sm">
+        <div class=" d-flex row mt-3 mb-3 container-xxl container-xl container-lg  container-md container-sm">
             <div class="col-xxl-1 col-xl-1 col-lg-1  "></div>
-            <div id="tabel_title"  class=" row col-xxl-10  col-xl-10 col-lg-10 border-box  d-flex align-items-center pe-5 ps-5">
+            <div id="tabel_title" class=" row col-xxl-10  col-xl-10 col-lg-10 border-box  d-flex align-items-center pe-5 ps-5">
                 <div class="col-xxl-4 col-xl-4 col-lg-5 table-title text-start"> Product </div>
                 <div class="col-xxl-3 col-xl-3 col-lg-2 ps-5 table-title text-start "> Price </div>
                 <div class="col-xxl-3 col-xl-3 col-lg-3 ps-5  table-title text-start "> Quantity </div>
@@ -28,9 +28,12 @@
             </div>
             <div class="col-xxl-1 col-xl-1 col-lg-1"></div>
         </div>
-    </div>
-    @component("components.cartlist")
-    @endcomponent
+        </div>
+
+        @foreach ($cartItems as $cartItem)
+        @component("components.cartItem", ["cartItem" => $cartItem])
+        @endcomponent
+        @endforeach
 
 
         <div class=" ms-auto notice-error "></div>
