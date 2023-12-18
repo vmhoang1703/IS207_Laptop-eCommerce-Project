4963d7f6ef474d55f56668c046c2859ba5891012
Hungpq13 <146099528+Hungpq13@users.noreply.github.com>
2023-12-18 08:19:56 +0700

    responsive footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
 <div class="container-fluid footer ">
     <div class="row pt-5 pb-4">
-        <div class="col-lg-6 col-md-6 col-sm-12 col-12 row name-footer ">
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12 row name-footer mb-5">
             <div class="offset-lg-1 offset-md-1 col-lg-7 col-md-7 col-sm-12 col-12 row text-center ">
                 <a href="#" class="col-lg-12 col-md-12 col-sm-8 col-8  ">
                     <img src="{{ asset('img/logo.jpg') }}" alt="logo" class="col-lg-12 col-md-12 col-sm-12 col-12 mb-2">
@@ -14,8 +14,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-6 ">
-            <ul class="footer_list">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-6 ">
+            <ul class="footer_list useless">
                 <li class="footer_list"><strong>Useful Links</strong></li>
                 <li class="footer_list">HOME</li>
                 <li class="footer_list">STORE</li>
@@ -23,7 +23,7 @@
                 <li class="footer_list">CONTACT</li>
             </ul>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-6 ">
+        <div class="col-lg-3 col-md-6    col-sm-6 col-6 ">
             <ul class="footer_list">
                 <li class="footer_list"><strong>Contact Us</strong></li>
                 <li class="footer_list">elecworld@gmail.com</li>
